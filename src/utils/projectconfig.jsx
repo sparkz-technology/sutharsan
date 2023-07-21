f4e452a29052da68a909ea33ba5e5d0c407aa605
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-07-21 16:05:43 +0530

    project~img

--- src/utils/projectconfig.jsx
+++ src/utils/projectconfig.jsx
@@ -3,7 +3,7 @@ const projectconfig = {
     {
       id: 1,
       name: "React Quiz",
-      img: "https://lh3.googleusercontent.com/pw/AIL4fc-crU0mcE7SMxLd-E-b8JAaVm5sO3fwMEMfvcJIS1iMc_8OuHtbDcNAz2V8O_-_FVb6uc3jv-rIoZJR7G5B1OSO1uRsUKkAMLXkRsedHb8WeckG9DuAdnCSuOfQSlpLUTKYHwGl0lnJ7pbQST-vwZhbOIB4aFZLpOnz_QIMMdueXfRZG9ZsVWEGIyNf52bG-9gVYlBTQwBT6_Rc57m6RkXPzo0xKHq-BM61drNvt3xN-6OgSEUg6aa5YrgF2mKPfXh8ozOhAHddVJKH8ohmma3NoA63-yL8tKucLiNfUA2wehxJEsxmEwRVjME7T7_oUHuhPkAn1829KYBH2Xg0dbvcG4nc0qzmYiVlTvh--X_CqV74SrbD0j4j7E1FG66Z-6BYKOpxKxnlgdLKey99WzoiK9ewu0mLzaFhbBPvHBM38if6CsaKrOTeT8qwngMrOgJ2s0rr7oGtWqUlKyZVl9TwryFZloqCsVVoI1Ki5MMdxFIaDAXqDQbfNHahsGmRTKyzOuDbIjiYgcWgmhz6JCpczQB1QID5-U88VNp91qrJiLgogp3BpBQCoWwjISaz8dkej9UF8BrbN9DNTWkW9iLhKXjtcb9cV_Yn5O0Y_bMcFMQephG5Hju32F4JaMB3lfWEyOprugI1Z5xlLmRFBGaNN-jfFJxpS9CkDcRC_fvwH39mZgT55heZtkSkaiF5dnrJPhcfrRUb_MEI7SjuZnwNU1zdYFm91YAe8aziJCEahSaJhI0xiZ3xD-5R1NAcUTc7JGzpOdNjXv_tbnj0jzxy3ZTDRFs1I6T8q5makfsWVr0BfECaK_pdN4oBCpna-T_3qewV0x8VNmmTL-pJxPClYtZuO7-QS3o0XjJoWMFnqtfkL1G1YXXGQKRm2Xags2ZW3ZAAV07E5mEDoG8AcR_MQP-Vv55aAdw2ogeWrzjl7Y5EHPMrAuO82q1wgkySQfqtoA=w949-h533-no?authuser=0",
+      img: "https://lh3.googleusercontent.com/pw/AIL4fc8jVLR270VbsqJYY2TIy-bypQsHiYkqF7JfCJWIaJwFGCCh1je0JPemxiRntW-rl44P-Zkdbx9bkt1PH3gEPZui-TogTLiC1YY5uKRwfvDeIoJ5IVChOJbksrOL4wxYSdoSVXoHRR_Kyv0PWGn_eXU=w1366-h669-s-no?authuser=0",
 
       subtitle:
         "This is a React-based quiz application that allows users to answer multiple-choice questions within a given time limit.",
@@ -14,7 +14,7 @@ const projectconfig = {
     {
       id: 2,
       name: "ToDo List",
-      img: "https://lh3.googleusercontent.com/pw/AIL4fc-crU0mcE7SMxLd-E-b8JAaVm5sO3fwMEMfvcJIS1iMc_8OuHtbDcNAz2V8O_-_FVb6uc3jv-rIoZJR7G5B1OSO1uRsUKkAMLXkRsedHb8WeckG9DuAdnCSuOfQSlpLUTKYHwGl0lnJ7pbQST-vwZhbOIB4aFZLpOnz_QIMMdueXfRZG9ZsVWEGIyNf52bG-9gVYlBTQwBT6_Rc57m6RkXPzo0xKHq-BM61drNvt3xN-6OgSEUg6aa5YrgF2mKPfXh8ozOhAHddVJKH8ohmma3NoA63-yL8tKucLiNfUA2wehxJEsxmEwRVjME7T7_oUHuhPkAn1829KYBH2Xg0dbvcG4nc0qzmYiVlTvh--X_CqV74SrbD0j4j7E1FG66Z-6BYKOpxKxnlgdLKey99WzoiK9ewu0mLzaFhbBPvHBM38if6CsaKrOTeT8qwngMrOgJ2s0rr7oGtWqUlKyZVl9TwryFZloqCsVVoI1Ki5MMdxFIaDAXqDQbfNHahsGmRTKyzOuDbIjiYgcWgmhz6JCpczQB1QID5-U88VNp91qrJiLgogp3BpBQCoWwjISaz8dkej9UF8BrbN9DNTWkW9iLhKXjtcb9cV_Yn5O0Y_bMcFMQephG5Hju32F4JaMB3lfWEyOprugI1Z5xlLmRFBGaNN-jfFJxpS9CkDcRC_fvwH39mZgT55heZtkSkaiF5dnrJPhcfrRUb_MEI7SjuZnwNU1zdYFm91YAe8aziJCEahSaJhI0xiZ3xD-5R1NAcUTc7JGzpOdNjXv_tbnj0jzxy3ZTDRFs1I6T8q5makfsWVr0BfECaK_pdN4oBCpna-T_3qewV0x8VNmmTL-pJxPClYtZuO7-QS3o0XjJoWMFnqtfkL1G1YXXGQKRm2Xags2ZW3ZAAV07E5mEDoG8AcR_MQP-Vv55aAdw2ogeWrzjl7Y5EHPMrAuO82q1wgkySQfqtoA=w949-h533-no?authuser=0",
+      img: "https://lh3.googleusercontent.com/pw/AIL4fc8498c5wXWqy35azX6FukVgoR5Qk7BVx--ZHdMNtvIPcVymEE96lDwCq0VtaiaiBY0A2_rzDL5-rLDblYHhDBeDzSbjst8PLNIu0juEE0QNdMV0-o4KM_v1S8WMXaFv-4qepeyKD1TlJqMF47hLesU=w1350-h664-s-no?authuser=0",
       subtitle:
         "This is a web app that lets users create and manage their to-do lists",
       technology: "EJS | Node.js | Express.js | MongoDB",
