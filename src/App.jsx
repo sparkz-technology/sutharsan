716f75cc7460e3ec042b207adebf800f921cd2a4
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-10 08:25:43 +0530

    admin-init

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import GlobalStyle from "./styles/GlobalStyle";
 import AppLayout from "./layout/AppLayout";
-import AdminLayout from "./layout/AdminLayout";
 import { DarkModeProvider } from "./features/Navbar/DarkModeContext";
 
 const routes = createBrowserRouter([
   { path: "/", element: <AppLayout />, errorElement: <h1>404</h1> },
-  { path: "/admin", element: <AdminLayout />, errorElement: <h1>404</h1> },
 ]);
 
 function App() {
