13cebd818778dfb096bdbb167af62abea8d4f72e
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2023-10-19 08:32:48 +0530

    Update AdminApi.jsx

--- src/services/AdminApi.jsx
+++ src/services/AdminApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://backend-peach-phi.vercel.app";
 
 // create a new axios instance
 const axiosInstance = axios.create({
