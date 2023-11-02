4fd97a5f3fe83961ee2e882b8e4e20468a0211af
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-11-02 20:56:38 +0530

    working

--- src/services/AdminApi.jsx
+++ src/services/AdminApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/";
-// const API_URL = "https://backend-peach-phi.vercel.app/";
+// const API_URL = "http://localhost:8000/";
+const API_URL = "https://backend-peach-phi.vercel.app/";
 
 
 const createAxiosInstance = () => {
